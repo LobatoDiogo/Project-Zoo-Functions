d0557cf137ff462f8f6e79660fd3b033d534a8c9
José Diogo Lobato Ribeiro <jdiogolobato@gmail.com>
2022-09-30 13:43:42 -0300

    Resolvi requisito 1

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,20 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Verifica se HandlerElephants é uma função', () => {
+    expect(typeof handlerElephants).toBe('function');
+  });
+
+  it('Verifica se ao não receber um parâmetro o retorno é undefined', () => {
+    expect(handlerElephants()).toBeUndefined();
+  });
+
+  it('Verifica se ao não receber um parâmetro em string o retorno é inválido', () => {
+    expect(handlerElephants(1)).toBe('Parâmetro inválido, é necessário uma string');
+    expect(handlerElephants({})).toBe('Parâmetro inválido, é necessário uma string');
+  });
+
+  it('Verifica se o retorno de HandlerElephants é uma string', () => {
+    expect(handlerElephants('13')).toBe('string');
+  });
+});
