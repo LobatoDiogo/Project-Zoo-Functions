9fae033cfa688106da41072f5e0e62290e9a1fe6
José Diogo Lobato Ribeiro <jdiogolobato@gmail.com>
2022-09-30 21:26:20 -0300

    Solução ao requisitos 2 e 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
-function getEmployeeByName(employeeName) {
-  // seu código aqui
+function getEmployeeByName(employeeName = null) {
+  if (employeeName === null) {
+    return {};
+  }
+  return employees.find((emp) => emp.firstName === employeeName || emp.lastName === employeeName);
 }
-
+console.log(getEmployeeByName('Nelson'));
 module.exports = getEmployeeByName;
+
+// Retorne um objeto vazio caso a função não receba parâmetros;
+
+// Retorne as informações da pessoa colaboradora caso o parâmetro seja igual ao nome ou igual ao último nome no seguinte formato:
+
+// A função deve retornar um objeto vazio caso não receba parâmetros;
+
+// A função deve retornar o objeto da pessoa colaboradora caso receba o seu primeiro nome como parâmetro;
+
+// A função deve retornar o objeto da pessoa colaboradora caso receba o seu último nome como parâmetro.
+
+// const animalSpecie = data.species.find((specie) => specie.name === animal);
